fdce6cb5fc850bcf23bebe3593948f35150bc924
casylm <casylm0619@gmail.com>
2024-04-28 22:10:04 +0900

    Initial commit

---
+++ src/class1/ClassStart1.java
@@ -0,0 +1,15 @@
+package class1;
+
+public class ClassStart1 {
+    public static void main(String[] args) {
+        String studentName1 = "학생1";
+        int age1 = 15;
+        int grade1 = 90;
+
+        String studentName2 = "학생2";
+        int age2 = 16;
+        int grade2 = 80;
+
+        System.out.println("이름: "+studentName1+" 나이: "+age1+" 성적: "+grade1);
+    }
+}
---
+++ src/class1/ex/MovieReivewMain1.java
@@ -0,0 +1,12 @@
+package class1.ex;
+
+public class MovieReivewMain1 {
+    public static void main(String[] args) {
+        MovieReivew movieReivew = new MovieReivew();
+
+        movieReivew.title = "인셉션";
+        movieReivew.review = "이직성공하고싶다ㅏㅏㅏㅏㅏ";
+        System.out.println("title:"+movieReivew.title);
+        System.out.println("review:"+movieReivew.review);
+    }
+}
